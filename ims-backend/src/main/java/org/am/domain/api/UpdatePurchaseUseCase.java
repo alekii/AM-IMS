f428b3614b15ab44ee462758c2a65b56e6cd4ef4
alekii <alexmuriithise@gmail.com>
2023-01-28 20:52:32 +0300

    Add Update Purchase Use Case

--- ims-backend/src/main/java/org/am/domain/api/UpdatePurchaseUseCase.java
+++ ims-backend/src/main/java/org/am/domain/api/UpdatePurchaseUseCase.java
@@ -1,11 +1,8 @@
 package org.am.domain.api;
 
-import org.am.domain.catalog.Product;
 import org.am.domain.catalog.Purchase;
-
-import java.util.List;
 
 public interface UpdatePurchaseUseCase {
 
-    Purchase update(final Purchase purchase, final List<Product> products);
+    Purchase update(final Purchase purchase);
 }
--- ims-backend/src/main/java/org/am/rest/services/PurchaseService.java
+++ ims-backend/src/main/java/org/am/rest/services/PurchaseService.java
@@ -3,8 +3,13 @@ package org.am.rest.services;
 import lombok.RequiredArgsConstructor;
 import org.am.domain.api.CreatePurchaseUseCase;
 import org.am.domain.api.GetPurchaseUseCase;
-import org.am.domain.api.UpdateProductUseCase;
+import org.am.domain.api.UpdatePurchaseUseCase;
+import org.am.domain.catalog.Purchase;
+import org.am.rest.services.responses.PurchaseResponse;
+import org.am.rest.services.responses.converters.PurchaseResponseConverter;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +19,28 @@ public class PurchaseService {
 
     private final GetPurchaseUseCase getPurchaseUseCase;
 
-    private final UpdateProductUseCase updateProductUseCase;
+    private final UpdatePurchaseUseCase updatePurchaseUseCase;
 
-  /*  public PurchaseResponse createPurchase(){
-        return createPurchaseUseCase.create()
-    }*/
+    private final PurchaseResponseConverter purchaseResponseConverter;
+
+    public PurchaseResponse create(final Purchase purchase) {
+
+        final Purchase createdPurchase = createPurchaseUseCase.create(purchase);
+
+        return purchaseResponseConverter.convert(createdPurchase);
+    }
+
+    public PurchaseResponse update(final Purchase purchase) {
+
+        final Purchase updatedPurchase = updatePurchaseUseCase.update(purchase);
+        
+        return purchaseResponseConverter.convert(updatedPurchase);
+    }
+
+    public PurchaseResponse getBySid(final UUID purchaseSid) {
+
+        Purchase purchase = getPurchaseUseCase.findBySid(purchaseSid);
+
+        return purchaseResponseConverter.convert(purchase);
+    }
 }

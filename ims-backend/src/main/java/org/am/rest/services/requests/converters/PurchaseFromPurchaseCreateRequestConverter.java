ff2d79c589b46697fab6267c3d57432a5cd0728a
alekii <alexmuriithise@gmail.com>
2023-01-29 22:11:05 +0300

    Add purchase create request

---
+++ ims-backend/src/main/java/org/am/rest/services/requests/converters/PurchaseFromPurchaseCreateRequestConverter.java
@@ -0,0 +1,46 @@
+package org.am.rest.services.requests.converters;
+
+import org.am.commons.utils.Converter;
+import org.am.domain.catalog.Product;
+import org.am.domain.catalog.Purchase;
+import org.am.domain.catalog.Supplier;
+import org.am.library.entities.util.PurchaseStatus;
+import org.am.rest.services.requests.PurchaseCreateRequest;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class PurchaseFromPurchaseCreateRequestConverter implements Converter<PurchaseCreateRequest, Purchase> {
+
+    @Override
+    public Purchase convert(PurchaseCreateRequest source) {
+
+        return Purchase.builder()
+                .invoice(source.getInvoice())
+                .sid(UUID.randomUUID())
+                .status(PurchaseStatus.PENDING_APPROVAL)
+                .supplier(convertSupplier(source.getSupplier()))
+                .products(convertProducts(source.getProducts()))
+                .dateReceived(source.getDateReceived())
+                .warehouseSid(source.getWarehouseSid())
+                .totalAmount(source.getTotalAmount())
+                .build();
+    }
+
+    private List<Product> convertProducts(List<Integer> products) {
+
+        return products.stream()
+                .map(product -> Product.builder()
+                        .id(product)
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    private Supplier convertSupplier(UUID supplier) {
+
+        return Supplier.builder()
+                .sid(supplier)
+                .build();
+    }
+}
